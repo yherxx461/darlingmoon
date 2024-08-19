47e751309a5b073246550e9c46b201e3ffaf09da
Ying Her <yher.prime@gmail.com>
2024-08-19 16:27:26 -0500

    Registration created successfully. Name, email, and phone added to UserPage Info.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -8,7 +8,12 @@ function UserPage() {
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+      {/* Commented out ID so that only can be see internally */}
+      {/* <p>Your ID is: {user.id}</p> */}
+      {/* Added name, email, and phone to personal UserPage. */}
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
       <LogOutButton className="btn" />
     </div>
   );
